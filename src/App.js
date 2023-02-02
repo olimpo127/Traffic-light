de17946d125d869daa7d3eabba6096fc321a6f1e
Felipe Ojeda <fojedai47@gmail.com>
2023-02-02 17:39:41 -0300

    first light

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const TrafficLight = () => {
+  const [light, setLight] = useState("red");
+
+  const changeLight = () => {
+    switch (light) {
+      case "red":
+        setLight("green");
+        break;
+      case "yellow":
+        setLight("red");
+        break;
+      case "green":
+        setLight("yellow");
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          backgroundColor: light === "red" ? "red" : "black",
+          display: "inline-block",
+          marginRight: "10px",
+        }}
+      />
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          backgroundColor: light === "yellow" ? "yellow" : "black",
+          display: "inline-block",
+          marginRight: "10px",
+        }}
+      />
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          backgroundColor: light === "green" ? "green" : "black",
+          display: "inline-block",
+        }}
+      />
+      <br />
+      <button onClick={changeLight}>Change Light</button>
     </div>
   );
-}
+};
 
-export default App;
+export default TrafficLight;
