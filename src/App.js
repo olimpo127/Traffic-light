e0c2923c1bef7c97fdc0aae1a7a334bb3b08cede
Felipe Ojeda <fojedai47@gmail.com>
2023-02-03 10:32:15 -0300

    finished lights

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import React, {useState} from "react";
 
 const TrafficLight = () => {
   const [light, setLight] = useState("red");
-
-  
+ 
 
   return (
     <div className='d-flex justify-content-center'>
       <div className='bg-dark col-1 d-flex flex-column m-5 p-1'>
       <div
         onClick={() => setLight("red")}
+        
         style={{
           height: "100px",
           width: "100px",
           borderRadius: "50%",
           backgroundColor: light === "red" ? "red" : "darkred",
+          boxShadow: light === "red" ? "10px 10px 10px 10px gold" : "none",
           display: "inline-block",
           marginRight: "10px",
         }}
@@ -29,6 +30,7 @@ const TrafficLight = () => {
           backgroundColor: light === "yellow" ? "yellow" : "olive",
           display: "inline-block",
           marginRight: "10px",
+          boxShadow: light === "yellow" ? "10px 10px 10px 10px gold" : "none",
         }}
       />
       <div
@@ -39,6 +41,7 @@ const TrafficLight = () => {
           borderRadius: "50%",
           backgroundColor: light === "lime" ? "lime" : "green",
           display: "inline-block",
+          boxShadow: light === "lime" ? "10px 10px 10px 10px gold" : "none",
         }}
       />
     </div>
